a8daff0d303f086d5b260d68fc0e95fd7a62b66b
crzyHead <37449089+crzyHead@users.noreply.github.com>
2018-03-28 00:12:03 +0300

    Add files via upload

---
+++ lab1-task6/Program.cs
@@ -0,0 +1,143 @@
+﻿/*
+        Не реализовал наследование, поскольку не нашел методов передачи массива для определения является ли равносторонним треугольник, 
+    а так же потому что основную функцию наследующего класса уже выполнял родительский класс.
+        В этой версии программы реализован выбор между автозаполнением и ручным вводом. Создана проверка на равносторонность(+равнобокость), но не путём наследования.
+        Реализованный вариант: №6. Задание варианта: По трём точкам найти стороны, периметр, площадь и определить минимальную медиану 
+    (реализованно для всех треугольников в силу сложности и маловероятности получения равностороннего треугольника путем не намеренного ввода)
+    + проверка равносторонность/равнобокости и существования треугольника. Суммарное кол-во строк: 400 (не считая комментарий и разделители)
+                                                                                                                                        Лисовский М. А., ст. гр. РЗ-171
+ */
+using System;
+using System.Threading;
+namespace figure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int k = 1;
+            int choVar;
+            bool boolVar = false;
+            String x = "";
+            String y = "";
+            Triangle[] tr = new Triangle[n];
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("0 - автозаполнение, 1 - ручной ввод");
+                choVar = int.Parse(Console.ReadLine());
+                Console.Clear();
+                Console.WriteLine("Режим: " + (choVar == 1 ? "ручной ввод" : "автозаполнение"));
+                if (choVar == 1)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        tr[i] = new Triangle();
+                        Console.WriteLine("Треугольник № " + (i + 1));
+                        for (int j = 0; j < n; j++)
+                        {
+                            do
+                            {
+                                Console.WriteLine("Введите координату X (для вещественных чисел использовать запятую):");
+                                x = Console.ReadLine();
+                            } while (tr[i].setX(x, j) != true);
+                            do
+                            {
+                                Console.WriteLine("Введите координату Y (для вещественных чисел использовать запятую):");
+                                y = Console.ReadLine();
+                            } while (tr[i].setY(y, j) != true);
+                        }
+                    }
+                    boolVar = true;
+                }
+                else
+                {
+                    if (choVar == 0)
+                    {
+                        boolVar = true;
+                        int i = 0;
+                        int j = 1;
+                        int b = 2;
+                        tr[i] = new Triangle();
+                        tr[j] = new Triangle();
+                        tr[b] = new Triangle();
+                        //------------
+                        tr[i].setX2(i, 0);
+                        tr[i].setY2(i, 0);
+                        tr[i].setX2(j, (0.5));
+                        tr[i].setY2(j, (Math.Sqrt(3) / 2));
+                        tr[i].setX2(b, 1);
+                        tr[i].setY2(b, 0);
+                        //-------------
+                        tr[j].setX2(i, -3);
+                        tr[j].setY2(i, 0);
+                        tr[j].setX2(j, 0);
+                        tr[j].setY2(j, 3);
+                        tr[j].setX2(b, 0);
+                        tr[j].setY2(b, -3);
+                        //-------------
+                        tr[b].setX2(i, -6);
+                        tr[b].setY2(i, 1);
+                        tr[b].setX2(j, 2);
+                        tr[b].setY2(j, 4);
+                        tr[b].setX2(b, 2);
+                        tr[b].setY2(b, -6);
+                    }
+                    else { Console.WriteLine("Введено неверное число"); Thread.Sleep(2000); boolVar = false; Console.Clear(); }
+                }
+            } while (boolVar != true);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    tr[i].setSide(j);
+                }
+                tr[i].CalculatePerimeter(i);
+                tr[i].CalculateSquare(i);
+                tr[i].setMed();
+                tr[i].AngleA();
+                tr[i].AngleB();
+                tr[i].AngleC();
+                tr[i].setStrg();
+                if (tr[i].getS() > 0)
+                {
+                    k = 1;
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.WriteLine("Длина стороны " + k + ": " + tr[i].getSide(j) + " (см)");
+                        k++;
+                    }
+                    k = 1;
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.WriteLine("Точка " + k + ": (" + tr[i].getX(j) + ";" + tr[i].getY(j) + ")");
+                        k++;
+                    }
+                    Console.WriteLine("Периметр: " + tr[i].getP(i) + " (см)");
+                    Console.WriteLine("Площадь: " + tr[i].getS() + " (см. квадр.)");
+                    Console.WriteLine("Угол A : " + tr[i].getAngleA() + " (радиан)");
+                    Console.WriteLine("Угол B : " + tr[i].getAngleB() + " (радиан)");
+                    Console.WriteLine("Угол C : " + tr[i].getAngleC() + " (радиан)");
+                    Console.WriteLine(tr[i].retStrg());
+                }
+                else
+                {
+                    Console.WriteLine("Трeугольник не существует!");
+                }
+                Console.WriteLine("-------------------------------------");
+            }
+            int indexMin = 0;
+            int numbMin = 0;
+            for (int i = 1; i < n; i++)
+                for (int j = 1; j < n; j++)
+                    if (tr[i].retMed(j) < tr[indexMin].retMed(numbMin))
+                    {
+                        indexMin = i;
+                        numbMin = j;
+                    }
+            Console.WriteLine("\n\nТреугольник с минимальной медианой (" + tr[indexMin].retMed(numbMin) + ") находится на " + (indexMin + 1) + " позиции под номером " + (numbMin + 1));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab1-task6/Triangle.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace figure
+{
+    class Triangle
+    {
+        private double a, b, c, S;
+        private double[] M = new double[3];
+        private double[] P = new double[3];
+        private double[] side = new double[3];
+        private double[] x = new double[3];
+        private double[] y = new double[3];
+        private string strg = "Треугольник не является равносторонним";
+
+        public double[] Side { get => side; set => side = value; }
+        public double[] X { get => x; set => x = value; }
+        public double[] Y { get => y; set => y = value; }
+        public string Strg { get => strg; set => strg = value; }
+
+        public bool setX(string X, int i)
+        {
+            bool result = false;
+            try
+            {
+                result = ((double.TryParse(X, out double res)) ? true : false);
+            }
+            catch (Exception e)
+            { }
+            if (result != false)
+                this.X[i] = double.Parse(X);
+            return result;
+        }
+        public void setX2(int i, double X)
+        {
+            this.X[i] = Math.Round(X, 3);
+
+        }
+        public void setY2(int i, double Y)
+        {
+            this.Y[i] = Math.Round(Y, 3);
+        }
+        public bool setY(string Y, int i)
+        {
+            bool result2 = false;
+            try
+            {
+                result2 = ((double.TryParse(Y, out double res)) ? true : false);
+            }
+            catch (Exception e)
+            { }
+            if (result2 != false)
+                this.Y[i] = double.Parse(Y);
+            return result2;
+        }
+
+        public double getX(int i)
+        {
+            return this.X[i];
+        }
+        public double getY(int i)
+        {
+            return this.Y[i];
+        }
+
+        public Triangle(double[] x, double[] y, int i)
+        {
+
+        }
+        public void setSide(int i)
+        {
+            if (i < 2)
+            {
+                this.Side[i] = Math.Round(Math.Sqrt(Math.Pow(this.X[i] - this.X[i + 1], 2) + Math.Pow(this.Y[i] - this.Y[i + 1], 2)), 2);
+            }
+            else
+            {
+                this.Side[i] = Math.Round(Math.Sqrt(Math.Pow(this.X[i] - this.X[0], 2) + Math.Pow(this.Y[i] - this.Y[0], 2)), 2);
+            }
+        }
+        public double getSide(int i)
+        {
+            return this.Side[i];
+        }
+
+        public Triangle()
+        {
+        }
+
+        public void CalculatePerimeter(int j)
+        {
+            for (int i = 0; i < 3; i++)
+                this.P[j] += this.Side[i];
+            this.P[j] = Math.Round(this.P[j], 2);
+        }
+        public double getP(int i)
+        {
+            return this.P[i];
+        }
+        public void CalculateSquare(int i)
+        {
+            double p = this.P[i] / 2;
+            double k = p * (p - this.Side[0]) * (p - this.Side[1]) * (p - this.Side[2]);
+            this.S = Math.Round(Math.Sqrt(k), 2);
+        }
+        public double getS()
+        {
+            return this.S;
+        }
+        public void AngleA()
+        {
+            this.a = Math.Round(Math.Acos((this.Side[0] * this.Side[0] + this.Side[2] * this.Side[2] - this.Side[1] * this.Side[1]) / (2 * this.Side[0] * this.Side[2])), 2);
+        }
+        public void AngleB()
+        {
+            this.b = Math.Round(Math.Acos((this.Side[0] * this.Side[0] + this.Side[1] * this.Side[1] - this.Side[2] * this.Side[2]) / (2 * this.Side[0] * this.Side[1])), 2);
+        }
+        public void AngleC()
+        {
+            this.c = Math.Round(Math.Acos((this.Side[1] * this.Side[1] + this.Side[2] * this.Side[2] - this.Side[0] * this.Side[0]) / (2 * this.Side[1] * this.Side[2])), 2);
+        }
+        public double getAngleA()
+        {
+            return this.a;
+        }
+        public double getAngleB()
+        {
+            return this.b;
+        }
+        public double getAngleC()
+        {
+            return this.c;
+        }
+        public void setMed()
+        {
+            this.M[0] = Math.Round((Math.Sqrt(2 * Math.Pow(this.Side[0], 2) + 2 * Math.Pow(this.Side[1], 2) - Math.Pow(this.Side[2], 2)) / 2), 2);
+            this.M[1] = Math.Round((Math.Sqrt(2 * Math.Pow(this.Side[0], 2) + 2 * Math.Pow(this.Side[2], 2) - Math.Pow(this.Side[1], 2)) / 2), 2);
+            this.M[2] = Math.Round((Math.Sqrt(2 * Math.Pow(this.Side[1], 2) + 2 * Math.Pow(this.Side[2], 2) - Math.Pow(this.Side[0], 2)) / 2), 2);
+        }
+        public double retMed(int i)
+        {
+            return this.M[i];
+        }
+        public void setStrg()
+        {
+            double b;
+            b = Math.Round((Side[0] + Side[1] + Side[2]) / (3 * Side[0]), 1);
+            if (b == 1)
+            {
+                Strg = "Треугольник является равносторонним";
+            }
+            else
+            {
+                if (Side[0] == Side[1] || Side[0] == Side[2] || Side[1] == Side[2])
+                {
+                    Strg = "Треугольник является равнобоким";
+                }
+
+                else
+                {
+                    Strg = "Треугольник не является ни равносторонним ни равнобоким";
+                }
+            }
+        }
+        public string retStrg()
+        {
+            return Strg;
+        }
+    }
+}
